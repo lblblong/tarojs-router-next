84011b2d1baf4c02916a68673bd59d32b2c449b5
libeilong <beilong.li@myoffer.com>
2021-04-20 17:07:43 +0800

    再次测试

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   description: '最好的taro，小程序路由解决方案',
   mode: 'site',
   publicPath: process.env.NODE_ENV === 'production' ? '/tarojs-router-next/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/tarojs-router-next/' : '/',
   navs: [
     {
       title: '使用指南',
