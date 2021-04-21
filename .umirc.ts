32960669e7f87b1dc67072e8e349be097edc684b
libeilong <beilong.li@myoffer.com>
2021-04-21 16:11:53 +0800

    doc: 文档添加示例

--- .umirc.ts
+++ .umirc.ts
@@ -19,6 +19,13 @@ export default defineConfig({
       path: '/api',
     },
     {
+      title: '示例',
+      children: [
+        { title: '查看代码', path: 'https://github.com/lblblong/tarojs-router-next/tree/master/examples' },
+        { title: '开发者工具打开', path: 'https://developers.weixin.qq.com/s/2CcFkJmo7Dpb' },
+      ],
+    },
+    {
       title: 'GitHub',
       path: 'https://github.com/lblblong/tarojs-router-next',
     },
