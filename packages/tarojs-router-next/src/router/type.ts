c2242fe119cc934d46171d9c5e43c0232cdb947c
lblblong <1162275271@qq.com>
2021-08-09 15:14:52 +0800

    feat: 中间件访问data参数、跳转类型

--- packages/tarojs-router-next/src/router/type.ts
+++ packages/tarojs-router-next/src/router/type.ts
@@ -7,13 +7,13 @@ export interface Route<E = any> {
 
 export enum NavigateType {
   /** 保留当前页面，跳转到应用内的某个页面。但是不能跳到 tabbar 页面。使用 Router.back 可以返回到原页面。小程序中页面栈最多十层。 */
-  navigateTo,
+  navigateTo = 'navigateTo',
   /** 关闭当前页面，跳转到应用内的某个页面。但是不允许跳转到 tabbar 页面。 */
-  redirectTo,
+  redirectTo = 'redirectTo',
   /** 关闭所有页面，打开到应用内的某个页面 */
-  reLaunch,
+  reLaunch = 'reLaunch',
   /** 跳转到 tabBar 页面，并关闭其他所有非 tabBar 页面 */
-  switchTab,
+  switchTab = 'switchTab',
 }
 
 export interface NavigateOptions {
