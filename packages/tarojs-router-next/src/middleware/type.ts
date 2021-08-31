3c84b280a73acb210f290c27d35df5e9a321848c
lblblong <1162275271@qq.com>
2021-08-31 10:40:11 +0800

    fix: 添加中间件中获取跳转类型（type），data的类型提示

--- packages/tarojs-router-next/src/middleware/type.ts
+++ packages/tarojs-router-next/src/middleware/type.ts
@@ -1,10 +1,14 @@
-import { Route } from '../router/type'
+import { NavigateType, Route } from '../router/type'
 
 export interface RouteContext<E = any> {
   /** 目标路由 */
   route: Route<E>
   /** 路由参数 */
   params: any
+  /** 跳转类型 */
+  type: NavigateType
+  /** 携带数据 */
+  data?: any
 }
 
 export type Middleware<E = any> = (ctx: RouteContext<E>, next: () => Promise<any>) => Promise<void>
